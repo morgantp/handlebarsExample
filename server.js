05bc507cf88f8741311c01faed6643588bae6ddc
Morgan <morgan.thomas-price@students.plymouth.ac.uk>
2020-03-26 12:38:42 +0000

    Added form and server feedback

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 var handlebars = require('express-handlebars');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var Contact = require('./models/Contact');
 
 app.set('view engine', 'hbs');
 app.engine('hbs', handlebars({
@@ -8,12 +11,44 @@ app.engine('hbs', handlebars({
     extname: 'hbs'
 }))
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
-    res.render('index', { layout: 'main'});
+    Contact.find({}).lean()
+        .exec((err, contacts) =>{
+            if(contacts.length){
+            res.render('index', { layout: 'main', contacts: contacts, contactsExist: true }); 
+            }else{
+            res.render('index', { layout: 'main', contacts: contacts, contactsExist: false });
+            }
+        })
 });
+
+app.post('/addContact', (req, res) =>{
+    const { name, email, number } = req.body;
+    var contact = new Contact({
+        name,
+        email,
+        number
+    });
+    contact.save();
+    res.redirect('/');
+})
 
 app.get('/about', (req, res) => {
     res.render('about', { layout: 'main'});
+});
+
+mongoose.connect('mongodb://localhost:27017/handlebars',{
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+})
+.then(() => {
+    console.log('Connected to DB');
+})
+.catch((err) => {
+    console.log('Not Connected to the DB with err : ' +err);
 });
 
 app.listen(3000, () => {
